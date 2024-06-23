399900c23aa004775c6a900ec07a02bb62242567
Ava Johnson <ava.johnson@zohomail.com>
2024-06-23 10:35:29 -0700

    Make patch code more readable

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,5 @@
-import { RoutePatch, ServerAPI } from 'decky-frontend-lib'
+import { RoutePatch, ServerAPI, afterPatch, wrapReactClass, wrapReactType } from 'decky-frontend-lib'
+import { ReactElement } from 'react'
 
 export const addPatch = (serverAPI: ServerAPI, route: string, patch: RoutePatch) => {
   const routePatch = serverAPI.routerHook.addPatch(route, patch)
@@ -6,3 +7,35 @@ export const addPatch = (serverAPI: ServerAPI, route: string, patch: RoutePatch)
 }
 
 export const hasProp = (prop: string) => (child: any) => prop in child.props
+
+export const patch = (object: any, ...resolvers: Array<(ret: ReactElement) => ReactElement | void>) => {
+  const handler = (_: unknown, ret: ReactElement) => {
+    const [fn, ...rest] = resolvers
+
+    const obj = fn(ret)
+
+    if (!rest.length) {
+      return obj ?? ret
+    }
+
+    if (obj) {
+      patch(obj, ...rest)
+    }
+
+    return ret
+  }
+
+  if (object.renderFunc) {
+    afterPatch(object, 'renderFunc', handler)
+  } else if (object.type.render) {
+    afterPatch(object.type, 'render', handler)
+  } else if (object.type.type) {
+    wrapReactType(object)
+    afterPatch(object.type, 'type', handler)
+  } else if (object.type.prototype?.render) {
+    wrapReactClass(object)
+    afterPatch(object.type.prototype, 'render', handler)
+  } else {
+    afterPatch(object, 'type', handler)
+  }
+}
--- src/libraryAppPatch.tsx
+++ src/libraryAppPatch.tsx
@@ -1,50 +1,25 @@
-import { wrapReactType, wrapReactClass, afterPatch } from 'decky-frontend-lib'
 import { ReactElement } from 'react'
-import { hasProp } from './helpers'
+import { hasProp, patch } from './helpers'
 import PlayerCount from './PlayerCount'
 
-const patchInfoSection = (section: any) => {
-  if (!section.props.children.find((child: any) => child.type === PlayerCount)) {
-    section.props.children.push(<PlayerCount />)
-  }
-}
-
 const libraryAppPatch = (props: { path: string; children: ReactElement }) => {
-  afterPatch(props.children.props, 'renderFunc', (_: unknown, ret1: ReactElement) => {
-    wrapReactType(ret1.props.children)
-    afterPatch(ret1.props.children.type, 'type', (_: unknown, ret2: ReactElement) => {
-      const appDetails = ret2.props.children?.[1]?.props.children.props.children.find(hasProp('children'))
-      wrapReactClass(appDetails.props.children)
-      afterPatch(appDetails.props.children.type.prototype, 'render', (_: unknown, ret3: ReactElement) => {
-        afterPatch(ret3.props.children.find(hasProp('overview')), 'type', (_: unknown, ret4: ReactElement) => {
-          afterPatch(ret4, 'type', (_: unknown, ret5: ReactElement) => {
-            afterPatch(ret5.props.children[0].type, 'render', (_: unknown, ret6: ReactElement) => {
-              afterPatch(ret6.props.children[0].type, 'render', (_: unknown, ret7: ReactElement) => {
-                const infoPanel = ret7.props.children.find(hasProp('overview'))
-                wrapReactClass(infoPanel)
-                afterPatch(infoPanel.type.prototype, 'render', (_: unknown, ret8: ReactElement) => {
-                  const infoItems = ret8.props.children.find(hasProp('className')).props.children[0]
-                  wrapReactClass(infoItems)
-                  afterPatch(infoItems.type.prototype, 'render', (_: unknown, ret9: ReactElement) => {
-                    patchInfoSection(ret9)
-                    return ret9
-                  })
-                  return ret8
-                })
-                return ret7
-              })
-              return ret6
-            })
-            return ret5
-          })
-          return ret4
-        })
-        return ret3
-      })
-      return ret2
-    })
-    return ret1
-  })
+  patch(
+    props.children.props,
+    (ret) => ret.props.children,
+    (ret) => ret.props.children?.[1]?.props.children.props.children.find(hasProp('children')).props.children,
+    (ret) => ret.props.children.find(hasProp('overview')),
+    (ret) => ret,
+    (ret) => ret.props.children[0],
+    (ret) => ret.props.children[0],
+    (ret) => ret.props.children.find(hasProp('overview')),
+    (ret) => ret.props.children.find(hasProp('className')).props.children[0],
+    (ret) => {
+      if (!ret.props.children.find((child: any) => child.type === PlayerCount)) {
+        ret.props.children.push(<PlayerCount />)
+      }
+    },
+  )
+
   return props
 }
 
